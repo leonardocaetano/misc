876295b0ece9f539778ed446281c9bc0bb14bf55
Leonardo Caetano <leonardocaetano27@gmail.com>
2025-10-26 15:06:43 -0300

    script

--- scripts/simple_qemu.sh
+++ scripts/simple_qemu.sh
@@ -2,12 +2,12 @@
 
 # to create the harddisk do the following command:
 # qemu-img create -f qcow2 <name>.qcow2 16G
-#
+# to run this without a terminal: nohup <script>.sh &
 
 qemu-system-x86_64 \
     -enable-kvm \
     -m 2048 \
     -nic user,model=virtio \
     -drive file=arch.qcow2,media=disk,if=virtio \
-        -cdrom arch.iso \
+    -cdrom arch.iso \
 #     -sdl
